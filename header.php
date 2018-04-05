2becc42bd9ea6a63fb7da8887af90a9973965b3d
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-04-05 07:36:01 +0200

    add CSP

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' 'unsafe-inline'">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
