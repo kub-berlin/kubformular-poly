f1f609dda7fd3d96ed0d9fc999b56c5de2e51800
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-04-21 13:26:15 +0200

    reference imprint and privacy

--- footer.php
+++ footer.php
@@ -9,11 +9,15 @@
 
 	<footer id="colophon" class="clearfix">
 		<p>
-			Proudly powered by <a href="http://wordpress.org/">WordPress</a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/en/themes/">Elmastudio</a>' ); ?>
-			<span class="sep"> | </span>
+			<!-- Proudly powered by <a href="http://wordpress.org/">WordPress</a> -->
+			<!-- <span class="sep"> | </span> -->
+			<!-- <?php printf( __( 'Theme: %1$s by %2$s', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/en/themes/">Elmastudio</a>' ); ?> -->
+			<!-- <span class="sep"> | </span> -->
 			Webdesign von <a href="http://www.claudia-klinger.de/webwork/">Claudia Klinger</a>
+			<span class="sep"> | </span>
+			<a href="https://kub-berlin.org/de/datenschutz" rel="privacy"><?php printf(_e('Privacy', 'yoko')) ?></a>
+			<span class="sep"> | </span>
+			<a href="https://kub-berlin.org/de/impressum"><?php printf(_e('Imprint', 'yoko')) ?></a>
 		</p>
 		<a href="#page" class="top"><?php _e( 'Top', 'yoko' ); ?></a>
 	</footer><!-- end colophon -->
